ccaac4c9ac84a41bf1ed5f73a6cd4e6082e7f9ca
Ethan <ethantheberg@gmail.com>
2023-01-21 14:16:26 -0700

    modular display method

--- core/src/com/mygdx/game/stragety.java
+++ core/src/com/mygdx/game/stragety.java
@@ -13,32 +13,8 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class stragety extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
 
-	@Override
-	public void create() {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render() {
-		ScreenUtils.clear(0, 0, 0, 1);
-		ShapeRenderer sr = new ShapeRenderer();
-		sr.begin(ShapeType.Filled);
-		sr.setColor(Color.RED);
-		sr.rect(0, 0, 300, 20);
-		sr.end();
-	}
-
-	@Override
-	public void dispose() {
-		batch.dispose();
-		img.dispose();
-	}
-
-	class node {
+	static class node {
 		int type;
 		Boolean occupied;
 
@@ -46,20 +22,37 @@ public class stragety extends ApplicationAdapter {
 			type = t;
 			occupied = false;
 		}
+
+		public void draw(int x, int y, SpriteBatch b) {
+			switch (type) {
+				case 0:
+					b.draw(new Texture(occupied ? "cone occupied.png" : "cone unoccupied.png"), x, y);
+					break;
+				case 1:
+					b.draw(new Texture(occupied ? "cube occupied.png" : "cube unoccupied.png"), x, y);
+					break;
+				case 2:
+					b.draw(new Texture(occupied ? "hybrid occupied.png" : "hybrid unoccupied.png"), x, y);
+					break;
+			}
+		}
 	}
 
-	class Grid {
+	static class Grid {
 		ArrayList<List<node>> nodes;
 		boolean coopbonus = false;
 
 		public Grid() {
 			nodes = new ArrayList<List<node>>();
-			nodes.add(Arrays.asList(new node(0), new node(1), new node(0), new node(0), new node(1), new node(0), new node(0),
-					new node(1), new node(0)));
-			nodes.add(Arrays.asList(new node(0), new node(1), new node(0), new node(0), new node(1), new node(0), new node(0),
-					new node(1), new node(0)));
-			nodes.add(Arrays.asList(new node(2), new node(2), new node(2), new node(2), new node(2), new node(2), new node(2),
-					new node(2), new node(2)));
+			nodes.add(
+					Arrays.asList(new node(0), new node(1), new node(0), new node(0), new node(1), new node(0), new node(0),
+							new node(1), new node(0)));
+			nodes.add(
+					Arrays.asList(new node(0), new node(1), new node(0), new node(0), new node(1), new node(0), new node(0),
+							new node(1), new node(0)));
+			nodes.add(
+					Arrays.asList(new node(2), new node(2), new node(2), new node(2), new node(2), new node(2), new node(2),
+							new node(2), new node(2)));
 		}
 
 		public int[] score() {
@@ -96,6 +89,42 @@ public class stragety extends ApplicationAdapter {
 			}
 			return new int[] { score, rp };
 		}
+
+		public void draw(SpriteBatch b) {
+			for (int i = 0; i < nodes.size(); i++) {
+				for (int j = 0; j < nodes.get(i).size(); j++) {
+					nodes.get(i).get(j).draw(j * 30, i * 30, b);
+				}
+			}
+		}
+	}
+
+	SpriteBatch batch;
+	Texture img;
+
+	@Override
+	public void create() {
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render() {
+		ScreenUtils.clear(0, 0, 0, 1);
+		ShapeRenderer sr = new ShapeRenderer();
+		Grid grid = new Grid();
+		batch.begin();
+		grid.draw(batch);
+		batch.end();
+		sr.begin(ShapeType.Filled);
+		sr.setColor(Color.RED);
+		sr.rect(0, 0, 300, 20);
+		sr.end();
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		img.dispose();
 	}
 
 	public int findPlacement() {
